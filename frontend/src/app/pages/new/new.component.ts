31f825d8a8f78381b3727c40aca9d3ad3ef3939a
pauromaoluche <pauluci125romao.pedro@hotmail.com>
2022-06-01 10:00:35 -0300

    Update: Add page new

--- frontend/src/app/pages/new/new.component.ts
+++ frontend/src/app/pages/new/new.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-new-food',
+  selector: 'app-new',
   templateUrl: './new.component.html',
   styleUrls: ['./new.component.scss']
 })
 export class NewComponent implements OnInit {
-
-  constructor() { }
+btnText = "Salvar"
+  constructor(public router: Router) { }
 
   ngOnInit(): void {
   }
