3a658cdcbf7eb9726bbf47e7c8567e76cbebb1d9
pauromaoluche <pauluci125romao.pedro@hotmail.com>
2022-06-01 10:02:18 -0300

    Update: started admin page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { NewComponent } from './pages/new/new.component';
+import { NewFoodComponent } from './components/new-food/new-food.component';
+import { NewGroupComponent } from './components/new-group/new-group.component';
+import { IndexComponent } from './admin/pages/index/index.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'novo', component: NewComponent}
+  {path: 'adicionar', component: NewComponent,
+children: [
+  {path: 'nova-comida', component: NewFoodComponent},
+  {path: 'novo-grupo', component: NewGroupComponent}
+]
+},
+{path: 'admin', component: IndexComponent},
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,17 +6,21 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
-import { FoodFormComponent } from './components/food-form/food-form.component';
 import { NewComponent } from './pages/new/new.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NewFoodComponent } from './components/new-food/new-food.component';
+import { NewGroupComponent } from './components/new-group/new-group.component';
+import { IndexComponent } from './admin/pages/index/index.component';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
     HomeComponent,
-    FoodFormComponent,
-    NewComponent
+    NewComponent,
+    NewFoodComponent,
+    NewGroupComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
